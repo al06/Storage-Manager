c1d66ea76542bf63e61263be767c83b3ce46220f
al06 <ankitluthra06@gmail.com>
2015-03-14 10:05:50 +0530

    Create index.php

---
+++ php-files/index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<img src="images/background.jpg" class="back" />
+
+
+<body>
+  
+  
+<div class="topnav">
+			<div id="header">
+            
+            	<div id="header_l">
+                
+                	<font face="Bradley Hand ITC" size="15" color="#fff"><b>
+                	SHARING DRIVE</b>
+                	</font>
+                
+                </div>
+                
+                <div id="header_r">
+                
+                	<div class="login">
+                        <form action="login.php" method="post">
+                        <label class="font"><font face="Bradley Hand ITC" ><b>Username</b></font></label>
+                        <input type="text" name="uname" placeholder="username" class="style"/>
+                        
+                        <label class="font"><font face="Bradley Hand ITC" ><b>Password</b></font></label>
+                        <input type="password" name="upass" placeholder="password" class="style"/>
+                        <input type="submit" class="sub" />
+                        </form>
+              		 </div>
+                
+                </div>
+                                
+            </div>
+
+</div>  
+  
+  
+  
+	<div id="main">
+    
+    	<div id="main_frame">
+        
+        	
+            
+            <div id="two">
+            
+            	<div id="two_l">
+                
+                	<img src="images/filesharing.jpg" />
+                
+                </div>
+                
+                <div id="two_r">
+                
+                	<div id="two_r_top">
+                    
+                    <font face="Bradley Hand ITC" size="+2" color="#0033FF"><b>
+                	If you are not a member REGISTER NOW</b></font>
+                    
+                    </div>
+                    
+                    <div id="two_r_down">
+                    
+                    	<div class="register">
+                            <form class="register" method="post" action="register.php">
+                             <label class="font"><font face="Bradley Hand ITC"><b>Username</b></font></label>
+                             
+                             <input type="text" name="uname" placeholder="username" required="required" class="style"/><br /><br />
+                             <label class="font"><font face="Bradley Hand ITC"><b>Password</b></font></label>
+                             
+                             <input type="password" name="upass" placeholder="password" required="required" class="style" /><br /><br />
+                             <label class="font"><font face="Bradley Hand ITC"><b>Name</b></font></label>
+                             
+                             <input type="text" name="name" placeholder="Name" required="required" class="style" /><br /><br />
+                             <label class="font"><font face="Bradley Hand ITC"><b>Email</b></font></label>
+                             
+                             <input type="email" name="email" placeholder="Email" required="required" class="style" /><br /><br />
+                            <input type="submit" class="sub"/>
+                            </form>
+						</div>
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
+           
+        
+        </div>
+    
+    </div>
+ 
+        <div class="topnav">    
+            <div id="footer"></div>
+        </div>
+
+</body>
+</html>
